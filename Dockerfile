d74b652c27b5b953767198072d4d3a10ad9afa07
Tuan Nguyen <nguyentranquoctuan1997@gmail.com>
2023-01-06 21:51:49 +0700

    hot reload through Air

--- Dockerfile
+++ Dockerfile
@@ -2,11 +2,11 @@ FROM golang:1.19
 
 WORKDIR /usr/src/app
 
-# pre-copy/cache go.mod for pre-downloading dependencies and only redownloading them in subsequent builds if they change
+#Hot reload through Air
+RUN curl -sSfL https://raw.githubusercontent.com/cosmtrek/air/master/install.sh | sh -s -- -b $(go env GOPATH)/bin
 COPY ./src/go.mod ./src/go.sum ./
 RUN go mod download && go mod verify
 
 COPY ./src .
-RUN go build -v -o /usr/local/bin/app ./...
 
-CMD ["app"]
+CMD ["air"]
--- src/main.go
+++ src/main.go
@@ -1,16 +1,46 @@
 package main
 
 import (
+	"encoding/json"
+	"log"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
+type TodoItem struct {
+	Id          int        `json:"id"`
+	Title       string     `json:"title"`
+	Description string     `json:"description"`
+	Status      string     `json:"status"`
+	CreatedAt   *time.Time `json:"created_at"`
+	UpdatedAt   *time.Time `json:"updated_at,omitempty"`
+}
+
 func main() {
 	r := gin.Default()
+
+	now := time.Now().UTC()
+
+	item := TodoItem{
+		Id:          1,
+		Title:       "task 1",
+		Description: "task 1 description",
+		Status:      "Doing",
+		CreatedAt:   &now,
+		// UpdatedAt:   nil,
+	}
+
+	jsData, err := json.Marshal(item)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
+			"message": string(jsData),
 		})
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
